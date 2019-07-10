dae36268ca3c57585a396523ecae9431771ac38e
ohlulu <ohlulu@goonsdesign.com>
2019-07-10 11:04:09 +0800

    [add] [engine]

---
+++ EnginePackge/OhEngine.py
@@ -0,0 +1,55 @@
+import datetime
+
+class UIImageEngine:
+
+    fileName = "OhImgae"
+
+    iamgeList = []
+
+    def __init__(self, fileName = "OhImgae"):
+        self.fileName = fileName
+
+    # private property or methods
+
+    # can add to many image
+    def addImage(self, name):
+        self.iamgeList.append(name)
+
+    def __imageStrf(self, imageName):
+        return f"    static let {imageName} = UIImage(named: {imageName})!"
+
+    def output(self, fileName):
+
+        file = open(f"./{fileName}.swift", "x")
+
+
+        today = datetime.datetime.today()
+
+        header = f"""
+//
+//  {self.fileName}.swift
+//  ohlulu
+//
+//  Created by OhImgae.py on {today.strftime("%Y/%m/%d")}
+//  Lastupdate on 2019/7/9
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+        """ 
+
+        result = """
+%(header)s
+import UIKit
+
+extension UIImage {\n
+"""
+
+        # print(result %  {"header": header})
+        file.write(result %  {"header": header})
+        for imageName in self.iamgeList:
+            # print(self.__imageStrf(imageName), "\n")
+            file.write(self.__imageStrf(imageName))
+            file.write("\n\n")
+                    
+        # print("}")
+        file.write("}")
+        file.close()
